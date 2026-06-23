9e2ce93281cd503e44b15f5a5bbccac4a2f3a521
CanhLai <24520925@gm.uit.edu.vn>
2026-06-23 21:02:53 +0700

    Cap nhat code Load Balancer hoat dong on dinh

--- src/DrawTogether.LoadBalancer/Program.cs
+++ src/DrawTogether.LoadBalancer/Program.cs
@@ -1,2 +1,33 @@
-// Diem khoi dong LoadBalancer.
-// Khoi tao ServerRegistry, LoadBalancerServer va HealthCheckService.
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using DrawTogether.LoadBalancer.Handlers;
+
+namespace DrawTogether.LoadBalancer
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int port = 8088;
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+
+            Console.WriteLine($"[Load Balancer] Dang chay tren cong {port}...");
+
+            // ---> KÍCH HOẠT BÁC SĨ CHẠY NGẦM Ở ĐÂY <---
+            _ = LoadBalancerHandler.StartHealthCheckAsync();
+            Console.WriteLine("[HealthCheck] He thong giam sat Server da bat!");
+
+            Console.WriteLine("[Load Balancer] San sang dieu phoi Client!\n");
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                LoadBalancerHandler handler = new LoadBalancerHandler(client);
+                _ = handler.HandleClientAsync();
+            }
+        }
+    }
+}
